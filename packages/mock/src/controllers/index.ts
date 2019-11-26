2e0e0bd54ed76a168fde9e888651c164187f6eac
wenyujie <wenyujie@hundun.cn>
2019-11-26 19:03:28 +0800

    📦 mock mock mock

--- packages/mock/src/controllers/index.ts
+++ packages/mock/src/controllers/index.ts
@@ -1,2 +1,14 @@
-var requireDirectory = require('require-directory');
-export default requireDirectory(module);
+import db from '../model/index'
+import common from '../common/index'
+const Mock = require('mockjs')
+class CommonController {
+    async commonHandler(req, res?): Promise<Object> {
+        return Mock.mock({
+            'list|1-10': [{
+                'id|+1': 1
+            }]
+        })
+    }
+}
+let commonController = new CommonController()
+export default commonController;
--- packages/mock/src/routers/index.ts
+++ packages/mock/src/routers/index.ts
@@ -1,22 +1,28 @@
-import orderController from '../controllers/order';
-console.log(orderController)
-const routers = [
-    {
-        path: '/',
-        method: 'GET',
-        handler: (request, h) => {
-            return h.response('pppppppppp').code(200);
-        }
-    },
-    {
-        path: '/order/add',
-        method: 'post',
-        handler: orderController.add
-    },
-    {
-        path: '/order/list/{pageNum}/{pageSize}',
-        method: 'get',
-        handler: orderController.list
-    }
-]
+import indexController from '../controllers/index';
+console.log(indexController)
+interface Router {
+    path: string
+    method: string
+    id: string
+    handler?: object
+}
+const ApiList: Router[] = [{
+    path: '/a',
+    method: 'GET',
+    id: '14'
+},{
+    path: '/b',
+    method: 'GET',
+    id: '13'
+},{
+    path: '/ab',
+    method: 'GET',
+    id: '12'
+}]
+
+const routers = ApiList.map(item => {
+    item.handler = indexController.commonHandler
+    delete item.id
+    return item
+})
 export default routers;
