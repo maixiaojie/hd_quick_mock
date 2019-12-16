9178c57972ef19e9430772277256fb45d3a01365
wenyujie <wenyujie@hundun.cn>
2019-12-16 15:23:21 +0800

    📦 feature: add token signature

--- packages/mock/src/auth/github.ts
+++ packages/mock/src/auth/github.ts
@@ -1,9 +1,27 @@
 
-// var JWT = require('jsonwebtoken'); // session stored as a JWT cookie
-
+var JWT = require('jsonwebtoken'); // session stored as a JWT cookie
+import db from '../model/mongo'
 const custom_handler = function (req, h, tokens, profile) {
     console.log(tokens, profile)
-    return 1
+    if(profile) {
+        var session = {
+            name: profile.name,
+            avatar_url: profile.avatar_url,
+            id: profile.id
+        }
+        var token = JWT.sign(session, 'maixiaojie')
+        return {
+            token: token,
+            userinfo: profile
+        }
+    }else {
+        return {
+            error_code: '401',
+            error_message: '登录失败'
+        }
+    }
+    
+    
 }
 
 export default custom_handler
